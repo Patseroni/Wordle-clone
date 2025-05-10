c06ed006bf26b7d05db98bd6bb19f5a5ed6083f2
Patseroni <patrik.bustad@lerniastudent.se>
2025-05-10 23:13:49 +0200

    fetch-function and filter words by lenght.

--- src/backend/api.js
+++ src/backend/api.js
@@ -1,25 +1,24 @@
-async function getData() {
+export async function fetchAllWords() {
     const response = await fetch('https://raw.githubusercontent.com/dwyl/english-words/refs/heads/master/words_dictionary.json');
     const data = await response.json();
-    return data;
+    return Object.keys(data);
 }
 
+export async function getWordByLength(length) {
+    try {
+        const allWords = await fetchAllWords();
+        const filteredWords = allWords.filter(word => word.length === length);
+        if (filteredWords.length > 0) {
+            const randomIndex = Math.floor(Math.random() * filteredWords.length);
+            return filteredWords[randomIndex];
+        }
+        else {
+            console.log('Cannot find word.');
+        }
 
+    }
+    catch {
+        console.error('Error when fetching word.', error.message);
+    }
 
-/*
-async function numberOfKeys() {
-    const data = await getData();
-    const nrOfKeys = Object.keys(data);
-    return nrOfKeys;
 }
-
-(async () => {
-    const keys = await numberOfKeys();
-    return keys;
-})();
-
-async function getRandomWord() {
-    const words = await getData();
-    const randomWord = math.random(Object.keys(words));
-}
-*/
